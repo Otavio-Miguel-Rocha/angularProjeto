67ed8f208f5566c0daa58ad19fa121639f15cc3e
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-04-18 21:54:51 -0300

    mudança e aprendizado de property bind com []()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'ProjetoAngular';
+export class AppComponent implements OnInit {
+  title = "ProjetoAngular";
+
+  ngOnInit(): void {
+    console.log("inicializado");
+  }
+  appBotaoClick(): void {
+    console.log(
+      "mouse passado por cima no app.botão! ESTAMOS NO APP.COMPONENT"
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BotaoModule } from "src/botao/botao.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BotaoModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
